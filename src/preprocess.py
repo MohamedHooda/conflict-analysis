f63157293e4e6dc090347b368e28a95b8a8c291a
rafiqkamel <kamel@encentive.de>
2024-02-11 04:12:42 +0100

    added formatting

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,6 +2,10 @@
 Module to clean and load data. 
 Functions starting with _ are not meant to be called directly but rather are helper functions for use inside the module
 """
+import string
+import os
+import langid
+import nltk
 import pandas as pd
 import re
 from bs4 import BeautifulSoup
@@ -11,14 +15,9 @@ from nltk.tokenize import RegexpTokenizer
 
 tokenizer = RegexpTokenizer(r'\w+')
 tokenizer.tokenize('Eighty-seven miles to go, yet.  Onward!')
-import nltk
-import string  
-import langid
 
 nltk.download('punkt')  # Download the necessary resources for tokenization
 
-import os
-import pandas as pd
 
 def merge_data_folder(folder_path):
     """
@@ -41,21 +40,23 @@ def merge_data_folder(folder_path):
             if file.endswith(".csv"):
                 # Create the full path to the CSV file
                 file_path = os.path.join(root, file)
-                
+
                 # Load the CSV file into a DataFrame
                 csv_data = pd.read_csv(file_path)
-                
+
                 # Concatenate the data to the main DataFrame
                 df = pd.concat([df, csv_data], ignore_index=True)
 
     return df
 
+
 def load_clean_data(path):
     """Loads cleaned data from a CSV file and parses column into their appropriate data types
     """
-    df = pd.read_csv(path, sep="|").drop(['Unnamed: 0'],axis=1)
+    df = pd.read_csv(path, sep="|").drop(['Unnamed: 0'], axis=1)
     df["dates"] = pd.to_datetime(df.dates)
     return df
+
 
 def clean_data(df):
     """
@@ -67,9 +68,9 @@ def clean_data(df):
     Returns:
         pd.DataFrame: clean dataframe
     """
-    #drop rows with null-value tweets
+    # drop rows with null-value tweets
     df.dropna(subset=["tweets"], inplace=True)
-    #drop duplicates
+    # drop duplicates
     df.drop_duplicates(subset=["tweet_id"], inplace=True, keep="first")
     # Apply the remove_html_tags function to the specified column
     df['tweets'] = df['tweets'].apply(_remove_html_tags)
@@ -94,13 +95,13 @@ def clean_data(df):
     df['leaning'] = df['leaning'].map(lambda x: x.replace(" ", ""))
     # make all tweets lowercase
     df['tweets'] = df['tweets'].map(lambda x: x.lower())
-    #remove any text between two these brackets <>
+    # remove any text between two these brackets <>
     df['tweets'] = df['tweets'].str.replace(r'<.*?>', '', regex=True)
     # Replace empty tweets with NaN
     df['tweets'].replace('', None, inplace=True)
-    #again drop rows with null-value tweets
+    # again drop rows with null-value tweets
     df.dropna(subset=["tweets"], inplace=True)
-    #remove non-english tweets 
+    # remove non-english tweets
     df = df[df["tweets"].apply(_detect_language)]
     # remove tweets that are just spaces
     df = df[~df['tweets'].str.isspace()]
@@ -109,32 +110,39 @@ def clean_data(df):
     df["dates"] = pd.to_datetime(df.dates, format=date_format)
     df["n_tokens"] = df["tweets"].apply(_count_tokens)
     return df
-    
+
+
 def cut_into_bins(df, column, bins, labels, new_col_name):
     """adds bins columns to a dataframe 
     """
-    df[new_col_name] = pd.cut(df[column], bins=bins, labels=labels, right=False)
+    df[new_col_name] = pd.cut(df[column], bins=bins,
+                              labels=labels, right=False)
     return df
-    
+
+
 def _detect_language(text):
     lang, confidence = langid.classify(text)
-    return lang == 'en'  # Check if the language is English  
- 
+    return lang == 'en'  # Check if the language is English
+
+
 def _remove_html_tags(text):
     soup = BeautifulSoup(text, 'html.parser')
-    return soup.get_text()   
- 
+    return soup.get_text()
+
+
 def _remove_links(text):
     # Define a regular expression to match URLs
     url_pattern = re.compile(r'https?://\S+|www\.\S+')
     # Replace URLs with an empty string
     return url_pattern.sub('', text)
 
+
 def _remove_domain_only_links(text):
     # Define a regular expression to match URLs
     url_pattern = re.compile(r'\.(com|net)\b')
     # Replace URLs with an empty string
     return url_pattern.sub('', text)
+
 
 def _remove_paths(text):
     """
@@ -150,6 +158,7 @@ def _remove_paths(text):
     cleaned_text = re.sub(r'\S*?/[^ ]*\b', '', text, flags=re.MULTILINE)
     return cleaned_text.strip()
 
+
 def _count_tokens(sentence):
     """
     Count the number of tokens in a sentence using NLTK word tokenization.
@@ -162,4 +171,3 @@ def _count_tokens(sentence):
     """
     tokens = nltk.word_tokenize(sentence)
     return len(tokens)
-
